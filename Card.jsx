ac52913ef069d08d35c6a677d0a345ed473b1f9d
User <ztemifavor@gmail.com>
2025-05-24 06:01:57 +0300

    added files

---
+++ Card.jsx
@@ -0,0 +1,14 @@
+import ProfilePic from './images/account.jpeg'
+
+function Card() {
+	
+
+	return <div className='card'>
+		<img src={ProfilePic} alt="profile picture" className='card-image'/>
+		<h2>User2</h2>
+		<p>I lear React and play football</p>
+	</div>
+		
+}
+
+export default Card;
